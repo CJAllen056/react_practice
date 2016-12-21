772ffe0e059e5c0d3ee3737fe47afa4b7f635c65
CJAllen056 <christopherjallen056@gmail.com>
2016-12-21 17:43:19 +0000

    video 2 done, basic router setup

--- react_native_fundamentals/index.ios.js
+++ react_native_fundamentals/index.ios.js
@@ -5,49 +5,34 @@
  */
 
 import React, { Component } from 'react';
+var Main = require('./App/Components/Main');
+
 import {
   AppRegistry,
   StyleSheet,
   Text,
+  NavigatorIOS,
   View
 } from 'react-native';
+
+var styles = StyleSheet.create({
+  container:{
+    flex: 1,
+    backgroundColor: '#111111'
+  }
+});
 
 export default class react_native_fundamentals extends Component {
   render() {
     return (
-      <View style={styles.container}>
-        <Text style={styles.welcome}>
-          Welcome to Github Notetaker!
-        </Text>
-        <Text style={styles.instructions}>
-          To get started, edit index.ios.js
-        </Text>
-        <Text style={styles.instructions}>
-          Press Cmd+R to reload,{'\n'}
-          Cmd+D or shake for dev menu
-        </Text>
-      </View>
+      <NavigatorIOS
+        style={styles.container}
+        initialRoute={{
+          title: "Github Notetaker",
+          component: Main
+        }} />
     );
   }
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#F5FCFF',
-  },
-  welcome: {
-    fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
-  },
-  instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
-  },
-});
 
 AppRegistry.registerComponent('react_native_fundamentals', () => react_native_fundamentals);
