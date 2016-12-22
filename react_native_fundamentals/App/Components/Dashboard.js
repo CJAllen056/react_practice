5ef1e11f0feda9edb60b967a2e27d5a307c1dec6
CJAllen056 <christopherjallen056@gmail.com>
2016-12-22 15:39:25 +0000

    video 8 done, dashboard page styled

--- react_native_fundamentals/App/Components/Dashboard.js
+++ react_native_fundamentals/App/Components/Dashboard.js
@@ -3,7 +3,9 @@ var React = require("react");
 import {
 	Text,
 	View,
-	StyleSheet
+	StyleSheet,
+	TouchableHighlight,
+	Image
 } from "react-native";
 
 var styles = StyleSheet.create({
@@ -22,11 +24,53 @@ var styles = StyleSheet.create({
 });
 
 class Dashboard extends React.Component{
+	makeBackground(btn) {
+		var obj = {
+			flexDirection: "row",
+			alignSelf: "stretch",
+			justifyContent: "center",
+			flex: 1
+		}
+
+		if (btn === 0) {
+			obj.backgroundColor = "#48BBEC"
+		} else if (btn === 1) {
+			obj.backgroundColor = "#E77AAE"
+		} else {
+			obj.backgroundColor = "#758BF4"
+		}
+
+		return obj;
+	}
+	goToProfile() {
+		console.log("going to profile Page");
+	}	goToRepos() {
+		console.log("going to repos Page");
+	}	goToNotes() {
+		console.log("going to notes Page");
+	}
 	render() {
 		return (
 			<View style={styles.container}>
-				<Text>This is the Dashboard</Text>
-				<Text> {JSON.stringify(this.props.userInfo)} </Text>
+				<Image source={{uri: this.props.userInfo.avatar_url}} style={styles.image} />
+				<TouchableHighlight 
+					style={this.makeBackground(0)}
+					onPress={this.goToProfile.bind(this)}
+					underlayColor="#88D4F5">
+					<Text style={styles.buttonText}>View Profile</Text>
+				</TouchableHighlight>
+				<TouchableHighlight 
+					style={this.makeBackground(1)}
+					onPress={this.goToRepos.bind(this)}
+					underlayColor="#88D4F5">
+					<Text style={styles.buttonText}>View Repos</Text>
+				</TouchableHighlight>
+				<TouchableHighlight 
+					style={this.makeBackground(2)}
+					onPress={this.goToNotes.bind(this)}
+					underlayColor="#88D4F5">
+					<Text style={styles.buttonText}>View Notes</Text>
+				</TouchableHighlight>
 			</View>
 		);
 	}
