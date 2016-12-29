d21726b672160d2a9a739d0275e2212e2846954d
CJAllen056 <christopherjallen056@gmail.com>
2016-12-29 19:03:19 +0000

    vid 16. responsiveness

--- data_driven_documents/src/app.js
+++ data_driven_documents/src/app.js
@@ -1,11 +1,15 @@
 var margin = { top: 10, right: 0, bottom: 60, left: 25 };
-var width = 425 - margin.left - margin.right;
-var height = 625 - margin.top - margin.bottom;
+var width = 400 - margin.left - margin.right;
+var height = 600 - margin.top - margin.bottom;
+
+var fullWidth = width + margin.left + margin.right;
+var fullHeight = height + margin.top + margin.bottom;
 
 var svg = d3.select(".chart")
   .append("svg")
     .attr("width", width + margin.left + margin.right)
     .attr("height", height + margin.top + margin.bottom)
+    .call(responsivefy)
   .append("g")
     .attr("transform", `translate(${margin.left}, ${margin.top})`);
 
@@ -31,3 +35,30 @@ svg
   .append("g")
     .attr("transform", `translate(0, ${height})`)
   .call(xAxis);
+
+function responsivefy(svg) {
+  // get container + svg aspect ratio
+  var container = d3.select(svg.node().parentNode),
+    width = parseInt(svg.style("width")),
+    height = parseInt(svg.style("height")),
+    aspect = width / height;
+
+  // add viewBox and preserveAspectRatio properties,
+  // and call resize so that svg resizes on inital page load
+  svg.attr("viewBox", "0 0 " + width + " " + height)
+    .attr("preserveAspectRatio", "xMinYMid")
+    .call(resize);
+
+  // to register multiple listeners for same event type,
+  // you need to add namespace, i.e., 'click.foo'
+  // necessary if you call invoke this function for multiple svgs
+  // api docs: https://github.com/mbostock/d3/wiki/Selections#on
+  d3.select(window).on("resize." + container.attr("id"), resize);
+
+  // get width of container and resize svg to fit it
+  function resize() {
+    var targetWidth = parseInt(container.style("width"));
+    svg.attr("width", targetWidth);
+    svg.attr("height", Math.round(targetWidth / aspect));
+  }
+}
