90c776198a2819de8bef377ce992bb28e655afb2
CJAllen056 <christopherjallen056@gmail.com>
2016-12-29 14:53:48 +0000

    vid 4. quantize scales

--- data_driven_documents/src/app.js
+++ data_driven_documents/src/app.js
@@ -1,5 +1,15 @@
-var timeScale = d3.scaleTime()
-  .domain([new Date(2016, 0, 1), new Date()])
-  .range([0, 100]);
+var quantizeScale = d3.scaleQuantize()
+  .domain([0, 100])
+  .range(["red", "orange", "yellow", "green"]);
 
-  console.log(timeScale.inver(50));
+// scaleQuantize returns output as value from set of inputs (in this case colours) These outputs are evenly split across the range
+
+console.log(quantizeScale(22));
+console.log(quantizeScale(49));
+console.log(quantizeScale(50));
+console.log(quantizeScale(88));
+console.log(quantizeScale(99));
+
+console.log(quantizeScale.invertExtent("yellow"));
+
+// returns the range that yellow represents, in this case 50-75
