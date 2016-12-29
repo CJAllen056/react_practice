b551944e53cf73c64159e918316f05d5482c71fa
CJAllen056 <christopherjallen056@gmail.com>
2016-12-29 15:06:26 +0000

    vid 6. loading and using data

--- data_driven_documents/src/app.js
+++ data_driven_documents/src/app.js
@@ -1,7 +1,24 @@
-var ordinalScale = d3.scaleOrdinal()
-  .domain(["poor", "good", "great"])
-  .range(["red", "yellow", "green"]);
+// d3.json loads the data. Can also use .csv or .tsv if data is in that format
 
-console.log(ordinalScale("poor"));
-console.log(ordinalScale("great"));
-console.log(ordinalScale("good"));
+d3.json('data/data.json', function (data) {
+
+  // d3.extent returns the range of data for a given value (in this case age) d3.max and d3.min can also be used
+
+  var extent = d3.extent(data, function (d) {
+    return d.age;
+  });
+  console.log(extent);
+
+  // The value found above is used to map the ages within the data to a linear scale
+
+  var scale = d3.scaleLinear()
+    .domain(extent)
+    .range([0, 600]);
+    
+  console.log(scale(37));
+
+  var ages = d3.set(data, function (d) {
+    return d.age;
+  });
+  console.log(ages.values());
+});
