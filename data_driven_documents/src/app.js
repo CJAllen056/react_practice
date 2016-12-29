affbeaaa5c7fc5c23d80721a9ae6b62b9b91934e
CJAllen056 <christopherjallen056@gmail.com>
2016-12-29 14:56:44 +0000

    vid 5. ordinal scales

--- data_driven_documents/src/app.js
+++ data_driven_documents/src/app.js
@@ -1,15 +1,7 @@
-var quantizeScale = d3.scaleQuantize()
-  .domain([0, 100])
-  .range(["red", "orange", "yellow", "green"]);
+var ordinalScale = d3.scaleOrdinal()
+  .domain(["poor", "good", "great"])
+  .range(["red", "yellow", "green"]);
 
-// scaleQuantize returns output as value from set of inputs (in this case colours) These outputs are evenly split across the range
-
-console.log(quantizeScale(22));
-console.log(quantizeScale(49));
-console.log(quantizeScale(50));
-console.log(quantizeScale(88));
-console.log(quantizeScale(99));
-
-console.log(quantizeScale.invertExtent("yellow"));
-
-// returns the range that yellow represents, in this case 50-75
+console.log(ordinalScale("poor"));
+console.log(ordinalScale("great"));
+console.log(ordinalScale("good"));
